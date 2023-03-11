c01d627387ac68f56bac6bfdc4d28e442da247cd
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2023-03-11 10:50:33 -0800

    Initial Commit

---
+++ CS32_PnetPhlix/Movie.cpp
@@ -0,0 +1,8 @@
+//
+//  Movie.cpp
+//  CS32_PnetPhlix
+//
+//  Created by Gregory Payton on 3/11/23.
+//
+
+#include <stdio.h>
---
+++ CS32_PnetPhlix/Movie.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  CS32_PnetPhlix
+//
+//  Created by Gregory Payton on 3/11/23.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
